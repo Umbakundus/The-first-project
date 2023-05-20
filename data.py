bf0e384011423d727a7f11cf98b470044b2a7cd3
Umbakundus <cverny4@gmail.com>
2023-05-20 18:45:02 +0100

    New files main and data were created, also data file's recieved Menu and resources

---
+++ data.py
@@ -0,0 +1,112 @@
+MENU = {
+    "margherita": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "cheese": 15
+        },
+        "cost": 10
+    },
+    "italiana": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "cheese": 10,
+            "salami": 20,
+            "onion": 10,
+            "arugula": 10,
+        },
+        "cost": 14,
+    },
+    "funghi": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "mushrooms": 20,
+            "cheese": 10,
+        },
+        "cost": 12,
+    },
+    "spinaci": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "spinacy": 20,
+            "garlic": 10,
+            "cheese": 15,
+        },
+        "cost": 12,
+    },
+    "vegetariana": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "cheese": 15,
+            "mushrooms": 15, 
+            "onion": 10,
+            "corn": 10,
+        },
+        "cost": 12,
+    },
+    "fabrizio": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "ham": 20,
+            "cheese": 15,
+            "olives": 15,
+        },
+        "cost": 14,
+    },
+    "pirata": {
+        "ingredients": {
+            "dough": 1,
+            "tomato": 20,
+            "ham": 15,
+            "cheese": 15,
+            "arugula": 10,
+            "garlic": 10,
+        },
+        "cost": 13,
+    },
+    "pollo": {
+        "ingredients": {
+            "dough": 1,
+            "tomatopaste": 20,
+            "ham": 15,
+            "cheese": 10,
+            "onion": 10,
+            "garlic": 10,
+        },
+        "cost": 13,
+    },
+    "delivery": {
+        "additional": {
+            "box": 1,
+            "gas": 10,
+        },
+        "cost": 6,
+    }
+}
+
+resources = {
+    "dough": 50,
+    "tomatopaste": 1000,
+    "cheese": 500,
+    "ham": 300,
+    "onion": 150,
+    "garlic": 150,
+    "salami": 100,
+    "arugula": 100,
+    "mushrooms": 100,
+    "olives": 100,
+    "corn": 100,
+    "spinacy": 100,
+    "box": 20,
+    "gas": 200,
+}
+
+
+
+
+  
